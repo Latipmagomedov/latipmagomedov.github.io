a8a59370d76e66198cea1b3805cb7dc9f18faf15
Latip <37000009+Latipmagomedov@users.noreply.github.com>
2021-06-17 20:29:49 +0300

    Delete assets directory

--- assets/js/menu.js
+++
@@ -1,6 +0,0 @@
-// Menu
-const menuBtn = document.querySelector(".open-menu");
-const menu = document.querySelector(".header__mobile-nav");
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("header__mobile-nav-open");
-});
